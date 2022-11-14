aa57d721579ca18b1d66626723db4be8e1878e43
Brian Walton <brimus@gmail.com>
2022-11-13 19:24:19 -0500

    updated for python3

--- SI7021.py
+++ SI7021.py
@@ -43,6 +43,6 @@ cTemp = ((data0 * 256 + data1) * 175.72 / 65536.0) - 46.85
 fTemp = cTemp * 1.8 + 32
 
 # Output data to screen
-print ("Relative Humidity is : %.2f %%") %humidity
-print ("Temperature in Celsius is : %.2f C") %cTemp
-print ("Temperature in Fahrenheit is : %.2f F") %fTemp
+print ("Relative Humidity is : %.2f %%" %humidity)
+print ("Temperature in Celsius is : %.2f C" %cTemp)
+print ("Temperature in Fahrenheit is : %.2f F" %fTemp)
